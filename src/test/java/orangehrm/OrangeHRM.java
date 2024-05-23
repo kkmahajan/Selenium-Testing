d06133db36a32f41708d65cdf41fccff66a21e99
Kaustubh <kstbhmahajan@gmail.com>
2024-05-23 10:07:18 +0530

    Added delete & search test cases for employee with exception in delete test case

--- src/test/java/orangehrm/OrangeHRM.java
+++ src/test/java/orangehrm/OrangeHRM.java
@@ -17,6 +17,10 @@ public class OrangeHRM {
 
 	public static String baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 	WebDriver chromeDriver;
+	String randomNumber;
+	String emplFName;
+	String empMName;
+	String empLName;
 
 	@BeforeTest
 	public void setup() {
@@ -59,16 +63,46 @@ public class OrangeHRM {
 	public void addEmployeeWithoutLoginDetailsTest() {
 
 		Random random = new Random();
-		String randomNumber = String.valueOf(random.nextInt(100));
+		randomNumber = String.valueOf(random.nextInt(100));
+		emplFName = "John" + randomNumber;
+		empMName = "K";
+		empLName = "Doe" + randomNumber;
 		chromeDriver.findElement(By.xpath("//span[normalize-space()='PIM']")).click();
 		chromeDriver.findElement(By.xpath("//a[normalize-space()='Add Employee']")).click();
-		chromeDriver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("John" + randomNumber);
-		chromeDriver.findElement(By.xpath("//input[@placeholder='Middle Name']")).sendKeys("K" + randomNumber);
-		chromeDriver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Doe" + randomNumber);
+		chromeDriver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(emplFName);
+		chromeDriver.findElement(By.xpath("//input[@placeholder='Middle Name']")).sendKeys(empMName);
+		chromeDriver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(empLName);
+		chromeDriver.findElement(By.xpath("//button[normalize-space()='Save']")).click();
+		String expectedElement = chromeDriver.findElement(By.xpath("//h6[normalize-space()='Personal Details']")).getText();
+		Assert.assertEquals(expectedElement, "Personal Details", "Validating that the personal details page is visible after adding employee");
+	}
+	
+	@Test(priority = 4)
+	public void searchEmployeeUsingFName() {
+		chromeDriver.findElement(By.xpath("//span[normalize-space()='PIM']")).click();
+		chromeDriver.findElement(By.xpath("(//input[@placeholder='Type for hints...'])[1]")).sendKeys(emplFName);
 		chromeDriver.findElement(By.xpath("//button[@type='submit']")).submit();
+		chromeDriver.findElement(By.xpath("//span[@class='oxd-text oxd-text--span']"));
+		String recordFoundText = chromeDriver.findElement(By.xpath("//span[@class='oxd-text oxd-text--span']")).getText();
+		Assert.assertTrue(recordFoundText.contains(" Found"), "Validating that a record is found for the searched employee first name");
+	}
+	
+	@Test(priority = 5)
+	public void deleteEmployee() {
+		chromeDriver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[9]/div/button[1]")).click();
+		waitForNow();
+		chromeDriver.findElement(By.xpath("//*[@id=\"app\"]/div[3]/div/div/div/div[3]/button[2]")).click();
+		chromeDriver.findElement(By.xpath("//span[normalize-space()='PIM']")).click();
+		chromeDriver.findElement(By.xpath("(//input[@placeholder='Type for hints...'])[1]")).sendKeys(emplFName);
+		chromeDriver.findElement(By.xpath("//button[@type='submit']")).submit();
+		chromeDriver.findElement(By.xpath("//span[@class='oxd-text oxd-text--span']"));
+		String recordFoundText = chromeDriver.findElement(By.xpath("//span[@class='oxd-text oxd-text--span']")).getText();
+		System.out.println("Emp First Name : "+emplFName);
+		System.out.println("No Records Found Text : "+recordFoundText);
+		Assert.assertTrue(recordFoundText.contains("No Records Found"), "Validating that a record is not found for the searched employee first name after deletion");
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 6)
 	public void logoutTest() {
 		waitForNow();
 		chromeDriver.findElement(By.xpath("//p[@class='oxd-userdropdown-name']")).click();
