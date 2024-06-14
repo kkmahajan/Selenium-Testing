d314ab9fdb81891ca71f68076cf885922cbbd4df
Kaustubh <kstbhmahajan@gmail.com>
2024-06-14 21:34:19 +0530

    Log4jDemo.java update

--- src/test/java/log4jdemo/Log4jDemo.java
+++ src/test/java/log4jdemo/Log4jDemo.java
@@ -3,17 +3,17 @@ package log4jdemo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDateTime;
+
 public class Log4jDemo {
 
 	private static Logger logger = LogManager.getLogger();
 	public static void main(String[] args) {
 		
-		System.out.println("\n Hello World    !!!\n");
-		
-		logger.info("This is an info message");
-		logger.warn("This is warning message");
-		logger.fatal("This is fatal message");
-		logger.error("This is an error message");
+        logger.info("This is an info message added on {}", LocalDateTime.now());
+        logger.warn("This is warning message added on {}", LocalDateTime.now());
+        logger.fatal("This is fatal message added on {}", LocalDateTime.now());
+        logger.error("This is an error message added on {}", LocalDateTime.now());
 		
 		System.out.println("Log4j Test Completed");
 	}
